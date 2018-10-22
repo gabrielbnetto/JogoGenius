fe38bae3bb37cbd41d3f3d1af779e23e78d7e3c1
Gabriel <31756573@mackenzista.com.br>
2018-10-21 21:21:02 -0300

    Redirecionamento e instruções

--- Home.js
+++ Home.js
@@ -1,4 +1,20 @@
+var idade;
+var cond = true;
 function mudarPagina(){
-    alert("Ola")
-    setTimeout("location.href = 'file:///Users/gabrieldiasmarinho/Documents/Mackenzie-2%C2%BA:2018/Fatores%20de%20Sistemas/Projeto/Jogo/Jogo/Jogo.html';",1500);
+    setTimeout("location.href = './Jogo.html';",1500);
+}
+function lerInstrucoes(){
+    alert('Entrou')
+    idade = document.getElementById('idade').value;
+    if(idade == 1){
+        document.getElementById('idosos').style.display = 'none';
+        document.getElementById('adultos').style.display = 'none';
+    }else if(idade == 2){
+            document.getElementById('criancas').style.display = 'none';
+            document.getElementById('idosos').style.display = 'none';
+    }else if(idade == 3){
+        document.getElementById('criancas').style.display = 'none';
+        document.getElementById('adultos').style.display = 'none';
+    }
+    alert(idade)
 }
